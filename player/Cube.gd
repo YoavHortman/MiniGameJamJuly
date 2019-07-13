029fe91f0de2f5b590777aabe3cc003d06efcd51
Yoav <yoavhortman5@gmail.com>
2019-07-13 14:18:51 +0200

    Added player movement on grid

--- player/Cube.gd
+++ player/Cube.gd
@@ -1,37 +1,42 @@
 extends Area2D
 
-const STEP_SIZE = 3200;
-var motion := Vector2();
+const STEP_SIZE = 100;
 
-func _physicis_process():
-	var right = Input.is_action_pressed("ui_right");
-	var left =  Input.is_action_pressed("ui_left");
-	var down = Input.is_action_pressed("ui_down");
-	var up = Input.is_action_pressed("ui_up");
+func _physics_process(delta):
+	var right = Input.is_action_just_pressed("ui_right");
+	var left =  Input.is_action_just_pressed("ui_left");
+	var down = Input.is_action_just_pressed("ui_down");
+	var up = Input.is_action_just_pressed("ui_up");
+	var motion = Vector2(0,0);
 	if right:
 		motion.x = STEP_SIZE;
 	elif left:
 		motion.x = -STEP_SIZE;
-
-	if down:
+	elif down:
 		motion.y = STEP_SIZE;
 	elif up:
 		motion.y = -STEP_SIZE;
 	
 	var newPos = get_global_position() + motion;
 	
-	print(newPos);
+	if newPos.y >= max_border_y || newPos.y <= min_border_y:
+		motion.y = 0;
+		newPos.y = min(newPos.y, max_border_y);
+		newPos.y = max(min_border_y, newPos.y);
+	if newPos.x >= max_border_x || newPos.x < min_border_x:
+		motion.x = 0;
+		newPos.x = min(newPos.x, max_border_x);
+		newPos.x = max(min_border_x, newPos.x);
 	
 	set_global_position(newPos);
-	motion = Vector2(0,0);
 
 var max_border_y: int;
 var max_border_x: int;
 
-const min_border_x = 32;
+const min_border_x = STEP_SIZE / 2;
 var min_border_y;
 
 func _ready():
-	max_border_y = get_viewport_rect().size.y - 32;
-	min_border_y = (get_viewport_rect().size.y / 1.5) - 32;
-	max_border_x = get_viewport_rect().size.x - 32;
+	max_border_y = get_viewport_rect().size.y - (STEP_SIZE / 2);
+	min_border_y = STEP_SIZE / 2;
+	max_border_x = get_viewport_rect().size.x - (STEP_SIZE / 2);
