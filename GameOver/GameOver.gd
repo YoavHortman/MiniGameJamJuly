4007fd5ea0572f3cc6da075520f2b30699f42890
Yoav <yoavhortman5@gmail.com>
2019-07-13 17:46:44 +0200

    Added game over, fonts and some obstacles

---
+++ GameOver/GameOver.gd
@@ -0,0 +1,7 @@
+extends Control
+
+func _on_Retry_pressed() -> void:
+	get_tree().change_scene("res://matrix/matrix.tscn");
+
+func _on_Quit_pressed() -> void:
+	get_tree().quit()
---
+++ obstacles/key.gd
@@ -0,0 +1,4 @@
+extends Area2D
+
+func _on_Key_area_entered(area):
+	get_parent().queue_free();
